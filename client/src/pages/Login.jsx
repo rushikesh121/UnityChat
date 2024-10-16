bbe128dd0e42034ff7073155b7beb1ccb305ba1f
Rushikesh Raut <raturushi1213@gmail.com>
2024-10-16 21:21:45 +0530

    login page added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
+import React from "react"
+import Container from "@mui/material/Container"
+import Paper from "@mui/material/Paper"
 import { useState } from "react";
 import Typography from "@mui/material/Typography";
 import { TextField, Button } from "@mui/material";
